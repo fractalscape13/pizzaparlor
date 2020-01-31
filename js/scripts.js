e897ce991d25bba0aa7863b1f05cbc61e1212301
Joseph Wangemann <fractalscape13@gmail.com>
2020-01-31 09:25:40 -0800

    create constructor functions for pizza and salad and prototypes to update size selection and toppings added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
+//business logic
+function Pizza() {
+  this.size = size;
+  this.toppings = 0;
+}
+
+Pizza.prototype.addTopping = function() {
+  this.toppings++;
+}
+
+Pizza.prototype.addSize = function() {
+  this.size = //input of size selection
+}
+
+function Salad() {
+  this.size = size;
+  this.additions = 0;
+}
+
+Salad.prototype.addAddition = function() {
+  this.additions++;
+}
+
+Pizza.prototype.addSize = function() {
+  this.size = //input of size selection
+}
+
+
+//user interface
 $(document).ready(function() {
   $("form").submit(function() {
-    
+
   });
 });
