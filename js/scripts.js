f0267d48982b61bfeef987afc391a788eb25fd2e
Joseph Wangemann <fractalscape13@gmail.com>
2020-01-31 10:18:06 -0800

    add button click functionality to detail screens to hide and show price screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // //business logic
 function Pizza() {
-  this.size = size;
+  this.size = 0;
   this.toppings = 0;
 }
 
@@ -8,12 +8,12 @@ Pizza.prototype.addTopping = function() {
   this.toppings++;
 }
 
-Pizza.prototype.addSize = function() {
-  this.size = size;
+Pizza.prototype.addSize = function(input) {
+  this.size += input;
 }
 
 function Salad() {
-  this.size = size;
+  this.size = 0;
   this.additions = 0;
 }
 
@@ -21,14 +21,16 @@ Salad.prototype.addAddition = function() {
   this.additions++;
 }
 
-Pizza.prototype.addSize = function() {
-  this.size = size;
+Pizza.prototype.addSize = function(input) {
+  this.size += input;
 }
 
 
 //user interface
 var currentOrder = "";
+var currentSize = 0;
 $(document).ready(function() {
+  //click function for start screen (salad or pizza)
   $("#pizzaorder").on("click", "button", function() {
     $("#pizzaorder").hide();
     $("#saladorder").hide();
@@ -40,12 +42,27 @@ $(document).ready(function() {
     $("#saladdetailscreen").fadeIn();
   });
   
+
   
-  
-  // $("#pizzaform").submit(function() {
-  //   event.preventDefault();
-  // });
-  // $("#saladform").submit(function() {
-  //   event.preventDefault();
-  // });
+  //click function for pizza detail screen
+  $("#pizzaform").submit(function() {
+    event.preventDefault();
+    currentOrder = new Pizza();
+    currentSize = parseInt($("#pizzasize").val());
+    currentOrder.addSize(currentSize);
+    // currentOrder.addTopping();
+    $("#detailscreen").hide();
+    $("#pricescreen").fadeIn();
+
+  });
+
+
+  //click function for salad detail screen
+  $("#saladform").submit(function() {
+    event.preventDefault();
+    currentOrder = new Salad();
+
+    $("#saladdetailscreen").hide();
+    $("#pricescreen").fadeIn();
+  });
 });
